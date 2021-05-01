7bd1f92f5a1cec13e6e7716b840310d384d3c8eb
datagutt <datagutt@lekanger.no>
2021-05-01 23:23:21 +0200

    add vue-plausible analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src:'~/plugins/timeago.js' }
+    { src:'~/plugins/timeago.js' },
+    { src:'~/plugins/plausible.js' }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
