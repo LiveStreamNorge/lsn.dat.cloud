86bf4c594bfd17b2aa0c53b527d05a8012227dad
datagutt <datagutt@lekanger.no>
2021-05-01 20:17:28 +0200

    Switch to vue-timeago

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <v-container fluid tag="div">
-    <link href='https://fonts.googleapis.com/css2?family=Raleway&family=Montserrat&family=Bebas Neue&display=swap'
-          rel='stylesheet' type='text/css'>
     <link rel="prefetch" href="https://lsn-api.dat.cloud/streams">
 
     <v-row justify="center">
@@ -30,7 +28,7 @@
 
     <v-card tag="div" class="pa-2 lastUpdated" @click="update" outlined>
       <v-skeleton-loader :loading="$fetchState.pending" type="text" width="200px">
-        {{ $t("lastUpdated") }} {{ lastUpdatedString }}
+        {{ $t("lastUpdated") }}: <timeago :datetime="lastUpdated" :locale="this.$root.$i18n.locale"></timeago>
       </v-skeleton-loader>
     </v-card>
 
@@ -58,7 +56,6 @@ export default {
   data() {
     return {
       streamer_data: [],
-      lastUpdatedString: "never",
       lastUpdated: null,
 
       featuredStreamer: null,
@@ -67,7 +64,6 @@ export default {
       offline: [],
 
       updateInterval: null,
-      lastUpdatedTextInterval: null,
     };
   },
 
@@ -75,77 +71,9 @@ export default {
 
   async mounted() {
     this.updateInterval = setInterval(this.update, 5 * 60 * 1000);
-    this.lastUpdatedTextInterval = setInterval(() => this.lastUpdatedString = this.timeAgo(this.lastUpdated), 50 * 1000);
   },
 
   methods: {
-    getFormattedDate(date, prefomattedDate = false, hideYear = false) {
-      const MONTH_NAMES = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-      ];
-
-      const day = date.getDate();
-      const month = MONTH_NAMES[date.getMonth()];
-      const year = date.getFullYear();
-      const hours = date.getHours();
-      let minutes = date.getMinutes();
-
-      if (minutes < 10) {
-        // Adding leading zero to minutes
-        minutes = `0${minutes}`;
-      }
-
-      if (prefomattedDate) {
-        // Today at 10:20
-        // Yesterday at 10:20
-        return `${prefomattedDate} at ${hours}:${minutes}`;
-      }
-
-      if (hideYear) {
-        // 10. January at 10:20
-        return `${day}. ${month} at ${hours}:${minutes}`;
-      }
-
-      // 10. January 2017. at 10:20
-      return `${day}. ${month} ${year}. at ${hours}:${minutes}`;
-    },
-
-    timeAgo(dateParam) {
-      if (!dateParam) {
-        return null;
-      }
-
-      const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
-      const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
-      const today = new Date();
-      const yesterday = new Date(today - DAY_IN_MS);
-      const seconds = Math.round((today - date) / 1000);
-      const minutes = Math.round(seconds / 60);
-      const isToday = today.toDateString() === date.toDateString();
-      const isYesterday = yesterday.toDateString() === date.toDateString();
-      const isThisYear = today.getFullYear() === date.getFullYear();
-
-
-      if (seconds < 5) {
-        return 'just now';
-      } else if (seconds < 60) {
-        return `${seconds} seconds ago`;
-      } else if (seconds < 90) {
-        return 'about a minute ago';
-      } else if (minutes < 60) {
-        return `${minutes} minutes ago`;
-      } else if (isToday) {
-        return this.getFormattedDate(date, 'Today'); // Today at 10:20
-      } else if (isYesterday) {
-        return this.getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
-      } else if (isThisYear) {
-        return this.getFormattedDate(date, false, true); // 10. January at 10:20
-      }
-
-      return this.getFormattedDate(date); // 10. January 2017. at 10:20
-    },
-
     filterStreamers() {
       this.offline = [];
       this.online = [];
@@ -165,7 +93,6 @@ export default {
         this.filterStreamers();
 
         this.lastUpdated = new Date();
-        this.lastUpdatedString = this.timeAgo( this.lastUpdated );
 
       } catch (e) {
         console.error(e);
@@ -180,10 +107,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.v-application {
-   font-family: "Raleway", sans-serif !important;
- }
-
 .content {
   max-width: 600px;
 }
