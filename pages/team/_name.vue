9b4934391d539bd88a2aa88d2b94881a4e1ea402
datagutt <datagutt@lekanger.no>
2021-05-03 20:48:11 +0200

    Add team pages

--- pages/team/_name.vue
+++ pages/team/_name.vue
@@ -4,8 +4,25 @@
 
     <v-row justify="center">
       <v-col class="mt-n5 content" tag="v-card">
-        <stream-filter class="mt-n1 mb-n1" @filter-changed="filterChanged" />
+        <!--<stream-filter class="mt-n1 mb-n1" @filter-changed="filterChanged" />-->
         <v-container tag="div" class="mt-3">
+          <v-row align="center" class="flex-shrink-1 mt-4 mb-3 px-5 mt-auto">
+            <h2 class="text-h6" v-if="team && team.name">
+              <span>{{ $t("streamersInTeam") }}:</span>
+              <v-img
+                width="32px"
+                contain
+                eager
+                v-if="team.image"
+                :alt="team.name"
+                :title="team.name"
+                :src="team.image"
+                class="d-inline-block"
+                style="vertical-align: middle"
+              />
+              <span v-if="!team.image">{{ team.name }}</span>
+            </h2>
+          </v-row>
           <v-expand-transition>
             <featured-streamer
               v-if="featuredStreamer"
@@ -33,8 +50,6 @@
             <stream-list-group-skeleton v-if="$fetchState.pending" key="0" />
             <stream-list-group :streamers="offline" v-else key="1" />
           </transition>
-
-          <random-quote class="ma-0 pa-0 mt-4" />
         </v-container>
       </v-col>
     </v-row>
@@ -58,20 +73,29 @@
 
 <script>
 import StreamListGroup from "~/components/StreamListGroup";
-import StreamerEntry from "../components/StreamerEntry";
-import FeaturedStreamer from "../components/FeaturedStreamer";
-import StreamListGroupSkeleton from "../components/StreamListGroupSkeleton";
-import StreamListHeader from "../components/StreamListHeader";
-import RandomQuote from "../components/RandomQuote";
+import StreamerEntry from "../../components/StreamerEntry";
+import FeaturedStreamer from "../../components/FeaturedStreamer";
+import StreamListGroupSkeleton from "../../components/StreamListGroupSkeleton";
+import StreamListHeader from "../../components/StreamListHeader";
 
+import { AVAILABLE_TEAMS } from "../../utils/constants";
 export default {
   components: {
-    RandomQuote,
     StreamListGroupSkeleton,
     FeaturedStreamer,
     StreamerEntry,
     StreamListHeader,
     StreamListGroup,
+  },
+
+  computed: {
+    team: function team() {
+      const teamParam = this.$route.params.name;
+      const thisTeam = AVAILABLE_TEAMS.filter(function thisTeam(t) {
+        return t && t.value === teamParam;
+      });
+      return thisTeam ? thisTeam[0] : null;
+    },
   },
 
   data() {
@@ -104,17 +128,9 @@ export default {
         (x.live ? this.online : this.offline).push(x)
       );
 
-      let featuredStreamer = this.online
+      this.featuredStreamer = this.online
         .filter((x) => !!x.featuredRank)
-        .sort((l, r) => l.featuredRank - r.featuredRank);
-      // Get random streamer from top 3 featured
-      this.featuredStreamer =
-        featuredStreamer[
-          Math.floor(
-            Math.random() *
-              (featuredStreamer.length < 3 ? featuredStreamer.length : 3)
-          )
-        ];
+        .sort((l, r) => l.featuredRank - r.featuredRank)[0];
       this.online.sort((l, r) => r.viewers - l.viewers);
     },
 
@@ -141,14 +157,15 @@ export default {
         console.error(e);
       }
     },
-    filterChanged: async function filterChanged(activeFilters) {
-      this.activeServerFilters = Object.assign([], activeFilters);
-      await this.update();
-    },
   },
-
   async fetch() {
-    await this.update();
+    // This will load nothing if team is invalid
+    if (this.team) {
+      this.activeServerFilters = {
+        team: this.team.value,
+      };
+      await this.update();
+    }
   },
 };
 </script>
