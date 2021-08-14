ba7064eafb2879e4cb9b9e2f8f9c6135ca6929ef
datagutt <datagutt@lekanger.no>
2021-08-14 15:33:18 +0200

    Delete plausible.js

--- plugins/plausible.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import { VuePlausible } from 'vue-plausible'
-
-Vue.use(VuePlausible, {
-	domain: 'lsn.dat.cloud',
-	trackLocalhost: false,
-})
-
-Vue.$plausible.enableAutoPageviews() // optional
